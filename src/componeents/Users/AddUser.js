c82d278f03dcd5af335109a64dad598989761a58
Souvik Jha <jhasouvik4@gmail.com>
2023-02-13 13:10:33 +0530

    error handler

--- src/componeents/Users/AddUser.js
+++ src/componeents/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 
 import Card from '../UI/Card';
 import classes from './AddUser.module.css'
@@ -6,39 +6,57 @@ import Button from '../UI/Button';
 import ErrorModel from '../UI/ErrorModel';
 
 const AddUser = (props) => {
-    const [enteredUserName,setEnteredUserName] = useState("")
-    const [enteredAge,setEnteredAge] = useState('')
+    const [enteredUserName, setEnteredUserName] = useState("")
+    const [enteredAge, setEnteredAge] = useState('')
+    const [error, setError] = useState()
 
-    const addUserHandler = (event)=>{
+    const addUserHandler = (event) => {
         event.preventDefault()
-        if(enteredUserName.trim().length===0 || enteredAge.trim().length===0) return
-        if(+enteredAge<1) return
-        props.onAddUser(enteredUserName,enteredAge)
-        
+        if (enteredUserName.trim().length === 0 || enteredAge.trim().length === 0) {
+            setError({
+                title: 'incalid input',
+                message: 'please enter a vlid name and age (non-empty)'
+            })
+            return
+        }
+        if (+enteredAge < 1) {
+            setError({
+                title: 'incalid age',
+                message: 'please enter a vlid age (>0)'
+            })
+            return
+        }
+        props.onAddUser(enteredUserName, enteredAge)
+
         setEnteredUserName("")
         setEnteredAge("")
     }
 
-    const userNameChangeHandler = (event)=>{
+    const userNameChangeHandler = (event) => {
         setEnteredUserName(event.target.value)
     }
 
-    const ageChangeHandler = (event)=>{
+    const ageChangeHandler = (event) => {
         setEnteredAge(event.target.value)
     }
 
+    const errorHandler = () =>{
+        setError(null)
+    }
+
     return (
-        <ErrorModel title = 'An error occured'>
-        <Card className={classes.input}>
-        <form onSubmit={addUserHandler}>
-            <lable htmlFor="username"> Username</lable>
-            <input id="username" type="text" value={enteredUserName} onChange={userNameChangeHandler} />
-            <lable htmlFor="age"> Age (Years)</lable>
-            <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler}/>
-            <Button type="submit">Add User</Button>
-        </form>
-        </Card>
-        </ErrorModel>
+        <div>
+            {error && <ErrorModel title={error.title} message = {error.message} onConfirm={errorHandler}/>}
+                <Card className={classes.input}>
+                    <form onSubmit={addUserHandler}>
+                        <lable htmlFor="username"> Username</lable>
+                        <input id="username" type="text" value={enteredUserName} onChange={userNameChangeHandler} />
+                        <lable htmlFor="age"> Age (Years)</lable>
+                        <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler} />
+                        <Button type="submit">Add User</Button>
+                    </form>
+                </Card>
+        </div>
     )
 }
 
